f1730f507f03c6a548b57ca0dcab184204178e3b
luisg-portatil <luisgardaz13@gmail.com>
2026-05-28 14:39:49 -0500

    chore: pal admin

--- lab06/apps/offers/admin.py
+++ lab06/apps/offers/admin.py
@@ -1,17 +1,16 @@
+from apps.offers.models.Application import Application
+from apps.offers.models.Candidate import Candidate
+from apps.offers.models.Company import Company
+from apps.offers.models.Offer import Offer
+from apps.offers.models.Recruiter import Recruiter
+from apps.offers.models.Technology import Technology
+from apps.offers.models.User import User
 from django.contrib import admin
 
-from .models import (
-    Company,
-    Profile,
-    Technology,
-    Job,
-    Application,
-    Skill
-)
-
+admin.site.register(User)
+admin.site.register(Candidate)
+admin.site.register(Recruiter)
 admin.site.register(Company)
-admin.site.register(Profile)
+admin.site.register(Offer)
 admin.site.register(Technology)
-admin.site.register(Job)
 admin.site.register(Application)
-admin.site.register(Skill)
