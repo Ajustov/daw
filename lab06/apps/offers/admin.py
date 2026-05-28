1ae5d202e451c67d41a50923b00c02a7852b054c
luisg-portatil <luisgardaz13@gmail.com>
2026-05-28 15:01:37 -0500

    chore: tablas faltantes

--- lab06/apps/offers/admin.py
+++ lab06/apps/offers/admin.py
@@ -1,7 +1,9 @@
 from apps.offers.models.Application import Application
 from apps.offers.models.Candidate import Candidate
+from apps.offers.models.CandidateTechnology import CandidateTechnology
 from apps.offers.models.Company import Company
 from apps.offers.models.Offer import Offer
+from apps.offers.models.OfferTechnology import OfferTechnology
 from apps.offers.models.Recruiter import Recruiter
 from apps.offers.models.Technology import Technology
 from apps.offers.models.User import User
@@ -14,3 +16,5 @@ admin.site.register(Company)
 admin.site.register(Offer)
 admin.site.register(Technology)
 admin.site.register(Application)
+admin.site.register(CandidateTechnology)
+admin.site.register(OfferTechnology)
